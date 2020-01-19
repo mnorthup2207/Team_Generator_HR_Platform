cc8d1d5072fe1cbccd07abb1a1ddb9a4b2da31f8
McCabe Northup <mccabenorthup@gmail.com>
2020-01-19 12:58:01 -0700

    added develop and assets folders

---
+++ Develop-v2/lib/Engineer.js
@@ -0,0 +1,5 @@
+// The Engineer class `extends` from Employee, and should have these additional properties/behaviors:
+
+// github (GitHub username)
+// getGithub()
+// getRole() (Overridden to return 'Engineer')
