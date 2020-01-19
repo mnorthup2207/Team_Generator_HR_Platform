2e48b3105303cc770e60f5989ab1f644fe02d0b1
McCabe Northup <mccabenorthup@gmail.com>
2020-01-19 13:17:41 -0700

    updated developer file

--- Develop-v2/lib/Engineer.js
+++
@@ -1,5 +0,0 @@
-// The Engineer class `extends` from Employee, and should have these additional properties/behaviors:
-
-// github (GitHub username)
-// getGithub()
-// getRole() (Overridden to return 'Engineer')
